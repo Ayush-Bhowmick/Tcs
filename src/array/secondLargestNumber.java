052706b2e15600c16b2eb7c46ed31b6ecceb6d6a
Ayush Bhowmick <109886923+Ayush-Bhowmick@users.noreply.github.com>
2024-04-23 21:16:06 +0530

    int practice

--- src/array/secondLargestNumber.java
+++ src/array/secondLargestNumber.java
@@ -23,5 +23,13 @@ public class secondLargestNumber {
     public static void main(String[] args) {
     Scanner sc= new Scanner(System.in);
 
+        System.out.println("ENTER THE NUMBER OF ELEMENTS: ");
+        int n=sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter the elements: ");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i]= sc.nextInt();
+        }
+        System.out.println(" SECOND LARGEST NUMBER" +secondLargest(arr));
     }
 }
