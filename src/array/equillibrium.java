ae4c6daffe9671d44cec7a1a4ca813e1fe9a8aa4
Ayush Bhowmick <109886923+Ayush-Bhowmick@users.noreply.github.com>
2024-04-30 23:27:53 +0530

    int practice

--- src/array/equillibrium.java
+++ src/array/equillibrium.java
@@ -2,7 +2,7 @@ package array;
 
 public class equillibrium {
     int sum = 0; // initialize sum of whole array
-
+    int leftsum = 0; // initialize leftsum
     public static void main(String[] args) {
 
     }
