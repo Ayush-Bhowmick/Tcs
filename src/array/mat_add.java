f7ccb08bdf6667e43265b8a50445ad6829fc6d12
Ayush Bhowmick <109886923+Ayush-Bhowmick@users.noreply.github.com>
2024-05-06 17:10:03 +0530

    int practice

---
+++ src/array/mat_add.java
@@ -0,0 +1,14 @@
+package array;
+import java.util.*;
+public class mat_add {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter the no of r1- ");
+         int r1=sc.nextInt();
+         System.out.print("Enter the c1- ");
+          int c1=sc.nextInt();
+          int[][] ans= new int[r1][c1];
+
+
+    }
+}
